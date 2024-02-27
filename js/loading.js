b64d0652029ea104fabf01cf119fce647bf89a9d
Harveer Singh <142636952+Harveertooshiesty@users.noreply.github.com>
2024-02-27 17:56:16 +0000

    Tue, Feb 27, 2024, 9:56 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,14 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
-loadText.innerText = '${load}%'
-loadText.style.opacity
+loadText.innerText = `${load}%`
+loadText.style.opacity = 0.5
+bg.style.filter = "blur(10px)"
+
+function blurring(){
+    load = load + 1
+    
+}
 
 
